ea9144f5211e44c8c1d7f72cc16256e93f595bb2
ct2995677 <48548862+ct2995677@users.noreply.github.com>
2019-06-05 20:00:55 +0800

    Add files via upload

---
+++ MavenDemo3/src/main/Java/Hello.java
@@ -0,0 +1,16 @@
+import com.neuedu.pojo.User;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("asdasdasd");
+
+        User user = new User();
+        user.toString();
+        user.getName();
+
+    }
+
+
+
+}
